9866876462676065888909368546b5463e993db5
Dietrich Epp <depp@zdome.net>
2011-10-27 03:09:47 -0700

    Fix coordinate error in draw_rect

--- src/draw_rect.c
+++ src/draw_rect.c
@@ -5,7 +5,7 @@ void draw_rect(int x, int y, int w, int h, unsigned color)
 {
     unsigned char *vp = video_ptr;
     int vrb = video_rowbytes, vw = video_width, vh = video_height;
-    int x0 = x, x1 = x0 + w, y0 = vh - 1 - y, y1 = y0 + h, xi, yi;
+    int x0 = x, x1 = x0 + w, y1 = vh - 1 - y, y0 = y1 - h, xi, yi;
     if (y0 < 0) y0 = 0;
     if (y1 > vh) y1 = vh;
     if (x0 < 0) x0 = 0;
